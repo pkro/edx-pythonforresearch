d46606e258f49a353e171ec171009ce0c08b3d23
pkro <pkrombach@web.de>
2019-09-17 08:53:05 +0200

    built knn classifier

--- case_studies/classification/main.py
+++ case_studies/classification/main.py
@@ -9,8 +9,8 @@ import random
 from collections import Counter
 
 import numpy as np
-import scipy
-
+from scipy import stats as ss
+from matplotlib import pyplot as plt
 
 
 def distance(p1, p2):
@@ -30,5 +30,44 @@ def majority_vote(votes):
 def majority_vote_short(votes):
     return ss.mstats.mode(votes)[0]
 
-winner = majority_vote([1,1,2,3,3])
-print(winner)
+def find_nearest_neighbors(p, points, k=5):
+    distances = np.array([distance(x, p) for x in points])
+    indices = np.argsort(distances)
+    # closest = list(zip(points[indices[:k]], distances[indices[:k]]))
+    # return closest
+    return indices[:k]
+
+def knn_predict(p, points, outcomes, k=5):
+    ind = find_nearest_neighbors(p, points, k)
+    return majority_vote(outcomes[ind])
+
+def generate_synth_data(n = 50):
+    ''' generate two sets of points from bivariate distribution'''
+    class1_points = ss.norm(0,1).rvs((n,2))
+    class2_points = ss.norm(1,1).rvs((n,2))
+    points = np.concatenate((class1_points, class2_points), axis=0)
+    outcomes = np.concatenate((np.repeat(0,n), np.repeat(1,n)))
+    
+    return points, outcomes
+
+'''
+points = np.array( [(x,y) for x in range(1,4) for y in range(1,4)] )
+outcomes = np.array([0,0,0,0,1,1,1,1,1,1])
+
+p = np.array([1, 1.0])
+
+print(knn_predict(p, points, outcomes))
+
+plt.plot(points[:,0], points[:,1],'ro');
+
+plt.plot(p[0], p[1], 'go');
+'''
+
+
+n = 20
+points, outcomes = generate_synth_data(n)
+plt.figure()
+plt.plot(points[:n,0], points[:n,1],'ro')
+plt.plot(points[n:,0], points[n:,1],'bo')
+plt.show()
+print(outcomes)
