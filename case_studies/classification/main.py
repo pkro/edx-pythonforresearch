718c7ee653b2b2cc99e247c106fc66cdd36c178d
pkro <pkrombach@web.de>
2019-09-18 08:39:27 +0200

    plotted predictions for different values of k

--- case_studies/classification/main.py
+++ case_studies/classification/main.py
@@ -50,6 +50,47 @@ def generate_synth_data(n = 50):
     
     return points, outcomes
 
+def make_prediction_grid(predictors, outcomes, limits, h, k):
+    ''' classify each point in prediction grid'''
+    (x_min, x_max, y_min, y_max) = limits
+    xs = np.arange(x_min, x_max, h)
+    ys = np.arange(y_min, y_max, h)
+    xx, yy = np.meshgrid(xs, ys)
+    
+    prediction_grid = np.zeros(xx.shape, dtype=int)
+    for i,x in enumerate(xs):
+        for j,y in enumerate(ys):
+            p = np.array([x,y])
+            prediction_grid[j,i] = knn_predict(p, predictors, outcomes, k)
+    
+    return (xx, yy, prediction_grid)
+
+def plot_prediction_grid (xx, yy, prediction_grid, filename):
+    """ Plot KNN predictions for every point on the grid."""
+    from matplotlib.colors import ListedColormap
+    background_colormap = ListedColormap (["hotpink","lightskyblue", "yellowgreen"])
+    observation_colormap = ListedColormap (["red","blue","green"])
+    plt.figure(figsize =(10,10))
+    plt.pcolormesh(xx, yy, prediction_grid, cmap = background_colormap, alpha = 0.5)
+    plt.scatter(predictors[:,0], predictors [:,1], c = outcomes, cmap = observation_colormap, s = 50)
+    plt.xlabel('Variable 1'); plt.ylabel('Variable 2')
+    plt.xticks(()); plt.yticks(())
+    plt.xlim (np.min(xx), np.max(xx))
+    plt.ylim (np.min(yy), np.max(yy))
+    plt.savefig(filename)
+            
+(predictors, outcomes) = generate_synth_data()
+
+k=5; filename="knn5.pdf"; limits=(-3,4,-3,4); h=0.1
+(xx,yy, prediction_grid) = make_prediction_grid(predictors, outcomes, limits, h,k)
+
+plot_prediction_grid(xx,yy,prediction_grid, filename)
+
+k=50; filename="knn50.pdf"; limits=(-3,4,-3,4); h=0.1
+(xx,yy, prediction_grid) = make_prediction_grid(predictors, outcomes, limits, h,k)
+
+plot_prediction_grid(xx,yy,prediction_grid, filename)
+
 '''
 points = np.array( [(x,y) for x in range(1,4) for y in range(1,4)] )
 outcomes = np.array([0,0,0,0,1,1,1,1,1,1])
@@ -61,7 +102,7 @@ print(knn_predict(p, points, outcomes))
 plt.plot(points[:,0], points[:,1],'ro');
 
 plt.plot(p[0], p[1], 'go');
-'''
+
 
 
 n = 20
@@ -71,3 +112,4 @@ plt.plot(points[:n,0], points[:n,1],'ro')
 plt.plot(points[n:,0], points[n:,1],'bo')
 plt.show()
 print(outcomes)
+'''
